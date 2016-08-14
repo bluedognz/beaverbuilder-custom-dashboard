03abdd51e768728819fd3b3802dcb964b2e36ff5
Clark Marshall <director@bluedogwebdesign.com>
2016-08-14 19:18:53 +1000

    Version 0.3

--- bb-custom-dashboard.php
+++ bb-custom-dashboard.php
@@ -3,7 +3,7 @@
  * Plugin Name: Beaver Builder Custom Dashboard
  * Plugin URI: http://www.bluedogdiywebsites.com
  * Description: Customize the WordPress Dashboard using a Beaver Builder Template
- * Version: 0.2
+ * Version: 0.3
  * Author: Blue Dog Digital
  * Author URI: http://www.bluedogdiywebsites.com
  * License: GPL2
--- readme.txt
+++ readme.txt
@@ -8,6 +8,10 @@ Copyright 2016 Blue Dog Digital Marketing Pty Ltd
 Change Log
 *********
 
+0.3 - August 2016
+
+- CSS tweaks. Thanks David Waumsley!
+
 0.2 - July 2016
 - double shortcode error fixed
 
