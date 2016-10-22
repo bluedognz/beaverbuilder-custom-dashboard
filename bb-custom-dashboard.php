af02c0a614cb9e4012ede220da96bdc079e3a35a
Jason <jason@oldcitypress.com>
2016-10-21 22:55:39 -0400

    refactored code
    refactored to enqueue all necessary styles and scripts, as well as wrapped in a class to prevent conflicts

---
+++ bb-custom-dashboard.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Beaver Builder Custom Dashboard
+ * Plugin URI: http://www.bluedogdiywebsites.com
+ * Description: Customize the WordPress Dashboard using a Beaver Builder Template
+ * Version: 0.3
+ * Author: Blue Dog Digital
+ * Author URI: http://www.bluedogdiywebsites.com
+ * License: GPL2
+ * Text Domain: bbcd
+ */
+
+ 
+// If this file is called directly, abort.
+if ( ! defined('WPINC') ) {
+	die;
+}
+
+require('classes/class-bb-custom-dashboard.php');
